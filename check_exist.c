b5f0af87cfa5c83a40cb3decebf9e23a875195b5
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-13 12:50:15 +0200

    fixed command is path

--- check_exist.c
+++ check_exist.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 
-
 /**
  * check_exist - chwck if program is found in enviroment
  * @command: the program name
@@ -14,18 +13,28 @@ int check_exist(char *command, char **absolute_path)
 	struct stat buffer;
 	char cwd[BUFSIZ], *path;
 	char *path_tokens[BUFSIZ] = {NULL};
-	
+
 	found = 0;
 	*path_len = 0;
 	*absolute_path = NULL;
 
-
 	getcwd(cwd, BUFSIZ);
 	path = getpath(environ);
 	parse_input(path + 5, path_tokens, path_len);
-	
-	
-	for (i = 0 ; i <= *path_len ; i++){
+
+	/*check if command is absolute path*/
+	if (command[0] == '/' || command[0] == '.')
+	{
+		chdir(command);
+		if (stat(command, &buffer) == 0)
+		{
+			found = 1;
+			*absolute_path = command;
+		}
+		chdir(cwd);
+	}
+
+	for (i = 0 ; i <= *path_len && !found; i++){
 		if (stat(command, &buffer) == 0)
 		{
 			found = 1;
@@ -34,11 +43,11 @@ int check_exist(char *command, char **absolute_path)
 				found = 0;
 			break;
 		}
-		
+
 		if (path_tokens[i] != NULL)
 			chdir(path_tokens[i]);
 	}
-	
+
 	chdir(cwd);
 	clear_tokens(path_tokens, path_len);
 	free(path_len);
